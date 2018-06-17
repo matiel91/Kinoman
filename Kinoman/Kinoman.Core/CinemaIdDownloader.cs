0df98a35610906aaa39b461231fca01ba9852c5d
Mateusz Jelen <mateuszjelen@gmail.com>
2018-06-17 22:55:36 +0200

    Create GetId method in CinemaDownloader 8/10 test passed, there is an unsolved problem with polish sign

--- Kinoman/Kinoman.Core/CinemaIdDownloader.cs
+++ Kinoman/Kinoman.Core/CinemaIdDownloader.cs
@@ -8,9 +8,31 @@ namespace Kinoman.Core
 {
     public class CinemaIdDownloader
     {
+        public CinemaLocation Venue { get; }
+
+        public CinemaIdDownloader(CinemaLocation venueParsedJson)
+        {
+            Venue = venueParsedJson;
+        }
+
         public int GetId(string location)
         {
-            throw new NotImplementedException();
+            string idResult = "0";
+            List<Cinema> cinemaList = new List<Cinema>();
+            foreach (var item in Venue.Venues)
+            {
+                foreach (var cinema in item.Cinemas)
+                {
+
+
+                    //if (cinema.Search.Equals(location, StringComparison.OrdinalIgnoreCase))
+                    if(cinema.Search.Trim().Equals(location, StringComparison.OrdinalIgnoreCase))
+                    {
+                        return int.Parse(cinema.Id);
+                    }
+                }
+            }
+            throw new Exception("Something wrong in CinemaIdDownloader class");
         }
     }
 }

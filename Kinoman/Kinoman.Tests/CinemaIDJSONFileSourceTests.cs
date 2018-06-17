602880777061d3f59b00c19d0cb9525ea2604741
Mateusz Jelen <mateuszjelen@gmail.com>
2018-06-17 21:07:54 +0200

    Move CinemaIdDownloaderTests to separate class

--- Kinoman/Kinoman.Tests/CinemaIDJSONFileSourceTests.cs
+++ Kinoman/Kinoman.Tests/CinemaIDJSONFileSourceTests.cs
@@ -5,7 +5,7 @@ namespace Kinoman.Tests
 
 
     [TestFixture]
-    public class CinemaIDJSONFileSourceTests
+    public partial class CinemaIDJSONFileSourceTests
     {
         [Test]
         public void IsNotNullWhenIsGetFromFile()
@@ -16,28 +16,6 @@ namespace Kinoman.Tests
             var result = cut.GetJson();
             //Assert
             Assert.NotNull(result);
-        }
-
-
-        [TestCase("Bydgoszcz", 3)]
-        [TestCase("Czechowice-Dziedzice", 37)]
-        [TestCase("Elbląg", 2)]
-        [TestCase("Gdańsk", 4)]
-        [TestCase("Gdynia", 5)]
-        [TestCase("Jaworzno", 41)]
-        [TestCase("Katowice", 39)]
-        [TestCase("Kielce", 33)]
-        [TestCase("Warszawa Wola Park", 15)]
-        [TestCase("Zabrze", 20)]
-        public void IsGetProperID(string location, int properId)
-        {
-            //Arrange
-            var cut = new CinemaIdDownloader();
-
-            //Act
-            int result = cut.GetId(location);
-            //Assert
-            Assert.AreEqual(result, properId);
         }
     }
 }
---
+++ Kinoman/Kinoman.Tests/CinemaIdDonwloaderTests.cs
@@ -0,0 +1,34 @@
+﻿using Kinoman.Core;
+using NUnit.Framework;
+namespace Kinoman.Tests
+{
+    public partial class CinemaIDJSONFileSourceTests
+    {
+        [TestFixture]
+        public class CinemaIdDonwloaderTests
+        {
+            [TestCase("Bydgoszcz", 3)]
+            [TestCase("Czechowice-Dziedzice", 37)]
+            [TestCase("Elbląg", 2)]
+            [TestCase("Gdańsk", 4)]
+            [TestCase("Gdynia", 5)]
+            [TestCase("Jaworzno", 41)]
+            [TestCase("Katowice", 39)]
+            [TestCase("Kielce", 33)]
+            [TestCase("Warszawa Wola Park", 15)]
+            [TestCase("Zabrze", 20)]
+            public void IsGetProperID(string location, int properId)
+            {
+                //Arrange
+                var parsedJson = new CinemaIdJSONFileSource();
+                var cut = new CinemaIdDownloader(parsedJson.GetJson());
+
+                //Act
+                int result = cut.GetId(location);
+                //Assert
+                Assert.AreEqual(result, properId);
+            }
+
+        }
+    }
+}

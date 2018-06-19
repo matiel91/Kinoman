503bdde10b81fb1706bd9982053dacd227e8d68d
Mateusz Jelen <mateuszjelen@gmail.com>
2018-06-19 15:53:48 +0200

    Correct method GetJson() in CinemaJSONFIleSource class Problem with polish charcaters solved

--- Kinoman/Kinoman.Core/CinemaIdJSONFileSource.cs
+++ Kinoman/Kinoman.Core/CinemaIdJSONFileSource.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Kinoman.Core
 
@@ -12,12 +13,12 @@ namespace Kinoman.Core
     {
         public CinemaLocation GetJson()
         {
-                       
-            string json = File.ReadAllText(@"C:\WORK SPACE\Projekty\Kinoman\Kinoman\Kinoman.Tests\TestFiles\CinemaIDJson.json");
+
+            var json = File.ReadAllText(@"C:\WORK SPACE\Projekty\Kinoman\Kinoman\Kinoman.Tests\TestFiles\CinemaIDJson.json", Encoding.Default).ToString();
+            Console.WriteLine(json);
             var data = CinemaLocation.FromJson(json);
             return data;
-            
-            
+
         }
     }
 }

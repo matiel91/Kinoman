d0d13896b2c30db42b04ff80083a271dda364ee5
Mateusz Jelen <mateuszjelen@gmail.com>
2018-06-19 16:32:42 +0200

    Add Unit Test FilmDataDownloaderFileSourceTests

---
+++ Kinoman/Kinoman.Tests/FilmDataDownloaderFileSourceTests.cs
@@ -0,0 +1,25 @@
+﻿using Kinoman.Core;
+using NUnit.Framework;
+namespace Kinoman.Tests
+{
+    public partial class FilmDataDownloaderWebSourceTests
+    {
+        [TestFixture]
+        public class FilmDataDownloaderFileSourceTests
+        {
+            
+            [TestCase(2)]
+            [TestCase(20)]
+            [TestCase(5)]
+            public void IsNotNull(int cinemaId)
+            {
+                //Arrange
+                var cut = new FilmDataDownloaderFileSource();
+                //Act
+                var result = cut.Get(cinemaId);
+                //Assert
+                Assert.NotNull(result);
+            }
+        }
+    }
+}

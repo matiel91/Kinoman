2af87aa2907291380705a2bc6056fda316c206d5
Mateusz Jelen <mateuszjelen@gmail.com>
2018-06-19 15:51:42 +0200

    Create method GetJson() in CinemaIdJSONWebSource class

--- Kinoman/Kinoman.Core/CinemaIdJSONWebSource.cs
+++ Kinoman/Kinoman.Core/CinemaIdJSONWebSource.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,10 @@ namespace Kinoman.Core
     {
         public CinemaLocation GetJson()
         {
-            throw new NotImplementedException();
+            HttpClient client = new HttpClient();
+            var json = client.GetStringAsync(@"https://multikino.pl/data/locations/").Result;
+            var result = CinemaLocation.FromJson(json);
+            return result;
         }
     }
 }

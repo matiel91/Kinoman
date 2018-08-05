99d2fc98bcb659900b3a35e15e81c82c78e930b8
Mateusz Jelen - DELL <mateuszjelen@gmail.com>
2018-08-06 05:18:40 +0900

    Create ResultDisplayTest Test for result display, test base on file cinema_34.json

--- Kinoman/Kinoman.Tests/ResultDisplayTest.cs
+++ Kinoman/Kinoman.Tests/ResultDisplayTest.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Kinoman.Core;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,31 @@ using System.Threading.Tasks;
 
 namespace Kinoman.Tests
 {
+    [TestFixture]
     class ResultDisplayTest
     {
+        [TestCase]
+        public void IsDispalyProprlyForToday()
+        {
+            
+            var executionProcedureFileSource = new ExecutionProcedureFileSource();
+            var cut = new ResultDisplay(executionProcedureFileSource.Execute());
+            string result = cut.GenerateResult();
+
+            string expectedResult = "Niedziela 05 Sierpnia\n" +
+                "Ant-Man: OSA\n" +
+                "13:05\t15:40\t18:15\t20:50" +
+                "Iniemamocni 2" +
+                "10:10\t14:50" +
+                "Jak zostać czarodziejem" +
+                "10:20\t12:35" +
+                "Książe Czaruś" +
+                "11:05" +
+                "Mamma Mia! Here We Go Again" +
+                "12:50\t15:20\t17:50\t20:20" +
+                "Mission: Imposible-Fallout" +
+                "19:30";
+            Assert.AreEqual(expectedResult, result);
+        }
     }
 }

bd29ecfb8d4f15529e16cfdc0e9af633284824c4
Mateusz Jelen - DELL <mateuszjelen@gmail.com>
2018-08-06 03:21:07 +0900

    Add ExecutionProcedureFileSource class

---
+++ Kinoman/Kinoman.Core/ExecutionProcedureFileSource.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kinoman.Core
+{
+    public class ExecutionProcedureFileSource
+    {
+        public Venue Execute()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

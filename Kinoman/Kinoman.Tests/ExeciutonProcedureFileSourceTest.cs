dbfb0a8715a254f57d0249090e0684f2f3a517cd
Mateusz Jelen - DELL <mateuszjelen@gmail.com>
2018-08-05 14:44:46 +0900

    Add Test Classes ExecutionProcedureFileSourceTest.cs, ExecutionProcedureWebSoureTest.cs, ResultDisplayTest.cs

---
+++ Kinoman/Kinoman.Tests/ExeciutonProcedureFileSourceTest.cs
@@ -0,0 +1,30 @@
+﻿using Kinoman.Core;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kinoman.Tests
+{
+    [TestFixture]
+    public class ExeciutonProcedureFileSourceTest
+    {
+        [TestCase]
+        public void isNotEmpty()
+        {
+            //method should return Cinema object
+            var cut = new ExecutionProcedureFileSource();
+            Assert.IsNotNull(cut.Execute());        
+        }
+        [TestCase]
+        public void isInstanceOfVenue()
+        {
+            //method should return Cinema object
+            var cut = new ExecutionProcedureFileSource();
+            Assert.IsInstanceOf(typeof(Venue), cut.Execute());
+        }
+
+    }
+}

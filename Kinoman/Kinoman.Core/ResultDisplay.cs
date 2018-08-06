7046a7b7321f4e11a8a65a8a350fbe5f8529f828
Mateusz Jelen - DELL <mateuszjelen@gmail.com>
2018-08-07 04:40:08 +0900

    Add ResultDisplay class to project

---
+++ Kinoman/Kinoman.Core/ResultDisplay.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kinoman.Core
+{
+    public class ResultDisplay
+    {
+        private readonly Venue venue;
+
+        public ResultDisplay(Venue venue)
+        {
+            this.venue = venue;
+        }
+
+        public string GenerateResult()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
